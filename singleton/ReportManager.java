c0f726a7694ba58ca23912178289f6774e91f6f8
pablochacon-hub <pablochacon028@gmail.com>
2025-06-20 19:50:06 -0500

    SINGLETON
    Codigo para patron de diseño Singleton

--- singleton/ReportManager.java
+++ singleton/ReportManager.java
@@ -1,3 +1,22 @@
-  public static void main(String[] args) {
-    
-  }
+ class ReportManager {
+    private static ReportManager instance;
+
+    private ReportManager() {}
+
+    public static ReportManager getInstance() {
+        if (instance == null) {
+            instance = new ReportManager();
+        }
+        return instance;
+    }
+
+    public void generateAndSendReport(ReportCreator creator, Notifier notifier) {
+        Report report = creator.createReport();
+
+        // Decoración (puede ser dinámica)
+        Report decorated = new FontDecorator(new ColorDecorator(report));
+
+        decorated.generateContent();
+        notifier.send(decorated);
+    }
+}
